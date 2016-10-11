21289eadcf4f38a69561be4ad90521d608f80a77
Shaojung <shaojung@gmail.com>
2016-10-11 12:08:14 +0800

    Initial commit

---
+++ app/src/main/java/com/test/tt101102/MainActivity.java
@@ -0,0 +1,53 @@
+package com.test.tt101102;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                URL url = null;
+                InputStream inputStream;
+                try {
+                    url = new URL("http://www.google.com");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    inputStream = conn.getInputStream();
+                    ByteArrayOutputStream result = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) != -1) {
+                        result.write(buffer, 0, length);
+                    }
+                    String str = result.toString("UTF-8");
+                    Log.d("NET", str);
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }.start();
+    }
+}
